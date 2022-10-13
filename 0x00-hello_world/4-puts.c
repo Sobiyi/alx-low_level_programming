14a3d32e1fc2bb5134585bd1d7f86317f624581d
Sobiyi <yetundesobiyi@gmail.com>
2022-10-13 13:18:33 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - Entry
- * Return: Always 0
+ * main - Printing
+ *
+ * Return: Always 0 (Successful)
  */
 int main(void)
-
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+}
