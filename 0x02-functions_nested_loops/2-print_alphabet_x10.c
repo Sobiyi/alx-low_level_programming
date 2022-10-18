80b539895a5e3a705fd048493b10a0fc731d85f5
Sobiyi <yetundesobiyi@gmail.com>
2022-10-18 16:29:31 -0700

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,7 @@
 #include "main.h"
+
 /**
- * print_alphabet_x10 - print 10 times thealhabet, in lowercase
+ * print_alphabet_x10 - print 10 times the alphabet, in lowercase
  */
 
 void print_alphabet_x10(void)
@@ -12,6 +13,6 @@ void print_alphabet_x10(void)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
-		_putchar('\n');
+		_putchar('/n');
 	}
 
