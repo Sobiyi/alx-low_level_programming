272a7990d200d70870baee735c2950faaa73b581
Sobiyi <yetundesobiyi@gmail.com>
2022-10-19 14:17:48 -0700

    task8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,14 +8,14 @@ void jack_bauer(void)
 {
 	int hour, minute;
 
-	for (hour = 0; hour <= 23; hours++)
+	for (hour = 0; hour <= 23; hour++)
 	{
 		for (minute = 0; minute <= 59; minute++)
 		{
-			_putchar(hour / 10) + '0');
-			_putchar(hour % 10) + '0');
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
 			_putchar(':');
-			_purchar((minute / 10) + '0'0);
+			_purchar((minute / 10) + '0');
 			_putchar((minute % 10) + '0');
 			_putchar('\n');
 
