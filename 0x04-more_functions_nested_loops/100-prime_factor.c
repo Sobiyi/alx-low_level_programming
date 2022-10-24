effff1c7ae13d7be1f85a0363841caab5eefa56f
Sobiyi <yetundesobiyi@gmail.com>
2022-10-24 14:11:43 -0700

    task11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - checks prime factors of 612852475143 and
+ * prints the largest;
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	long int num, a;
+
+	num = 612852475143
+
+	for (a = 2; a <= num; a++)
+	{
+		if (num % a == 0)
+		{
+			num = num / a;
+
+			a--;
+		}
+	}
+
+	printf("%ld\n", a);
+
+	return (0);
+}
