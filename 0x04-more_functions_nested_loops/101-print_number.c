ded8af46d68ded757a5fc90a3c0cdfbb1b88bec8
Sobiyi <yetundesobiyi@gmail.com>
2022-10-24 14:23:57 -0700

    task12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	unsigned int m;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		m = -n;
+	}
+	else
+	{
+		m = n;
+	}
+
+	if (m / 10)
+		print_number(m / 10);
+
+	_putchar(m % 10 + 48);
+}
