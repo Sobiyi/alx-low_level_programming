eebf8e58969813c400910fe73589e79973a874a0
Sobiyi <yetundesobiyi@gmail.com>
2022-11-02 13:53:31 -0700

    task8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,25 +1,5 @@
 #include "main.h"
-#include <string.h>
 
-/**
- * find_last_index - finds the last index of a charcter in a string
- * @s: a pointer to the string
- * @c: the character to search for
- * @st: where to start search
- *
- * Return: 0 if c is *, 1 if c is found and -1 otherwise
- */
-int find_last_index(char *s, char c, int st)
-{
-	if (c == '*')
-		return (0);
-	else if (s[st] == c)
-		return (st + 1);
-	else if (s[st] == '\0')
-		return (-1);
-	else
-		return (find_last_index(s, c, st + 1));
-}
 
 /**
  * wildcmp - compares two strings with wildcard *
@@ -30,39 +10,17 @@ int find_last_index(char *s, char c, int st)
  */
 int wildcmp(char *s1, char *s2)
 {
-	int r;
+	if (s1[0] == '\0' && s2[0] == '*' && s2[1] != '\0')
+		return (0);
 
 	if (s1[0] == '\0' && s2[0] == '\0')
-	{
 		return (1);
-	}
-	else if (s2[0] == '*')
-	{
-		if (s2[1] == '\0')
-			return (1);
 
-		r = find_last_index(s1, s2[1], 0);
-
-		if (r == -1)
-		{
-			return (0);
-		}
-		else if (r == 0)
-		{
-			return (wildcmp(s1, &s2[1]));
-		}
-		else
-		{
-			return (wildcmp(&s1[r], &s2[2]));
-		}
-	}
-	else if (s1[0] != s2[0])
-	{
-		return (0);
-	}
-	else
-	{
+	if (s1[0] == s2[0])
 		return (wildcmp(&s1[1], &s2[1]));
-	}
-		return (0);
+
+	if (s2[0] == '*')
+		return (wildcmp(&s1[0], &s2[1]) || wildcmp(&s1[1], &s2[0]));
+
+	return (0);
 }
